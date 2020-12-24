af2542c2aaa457062a4b5e30e5955ed1c8d63ed2
Pavel Ziryukin <zir-pav@mail.ru>
2020-12-24 17:59:54 +0300

    New port

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -2,9 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.service.MainService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Map;
@@ -23,5 +26,10 @@ public class MainController {
     public ResponseEntity<Map<Integer, Long>> getSimpleNumbers(@RequestBody List<Integer> list) {
         Map<Integer, Long> integerLongMap = mainService.getSimpleNumbers(list);
         return ResponseEntity.ok(integerLongMap);
+    }
+
+    @GetMapping
+    public ResponseEntity<String> getHi(@RequestParam String name) {
+        return new ResponseEntity<>("Hello, " + name, HttpStatus.OK);
     }
 }
